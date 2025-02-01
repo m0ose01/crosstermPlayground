2c73f3a5887d59193571ab39de08e4ab9b3effac
Seamus <m0ose01@proton.me>
2025-02-01 18:19:23 +0000

    Move to alternate screen.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-use std::io::stdout;
+use std::io::{self, Write};
 
 use crossterm::{
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand,
+    event,
+    cursor,
+    QueueableCommand,
+    terminal,
 };
 
 fn main() -> std::io::Result<()> {
-    stdout()
-        .execute(SetForegroundColor(Color::DarkRed))?
-        .execute(SetBackgroundColor(Color::DarkGrey))?
-        .execute(Print("hello, world\n"))?
-        .execute(ResetColor)?;
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout
+        .queue(terminal::EnterAlternateScreen)?
+        .queue(cursor::MoveTo(0, 0))?
+        .queue(cursor::Hide)?
+        .queue(terminal::Clear(terminal::ClearType::All))?
+        .queue(SetForegroundColor(Color::DarkRed))?
+        .queue(SetBackgroundColor(Color::DarkGrey))?
+        .queue(Print("hello, world\n"))?
+        .queue(ResetColor)?;
+
+    stdout.flush()?;
+
+    loop {
+        match event::read()? {
+            event::Event::Key(event) => {println!("{:?}", event);break;},
+            _ => (),
+        }
+    }
+
+    stdout
+        .queue(cursor::Show)?
+        .queue(terminal::LeaveAlternateScreen)?
+        .flush()?;
+
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
